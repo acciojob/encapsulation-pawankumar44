d05c2179238827835923c4193551a483ce800970
Pawan Kumar <pawankumar98>
2023-10-17 14:38:45 +0530

    assignment done

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,14 @@
 package com.driver;
 
 public class Main {
-  
+
+    public static void main(String[] args) {
+        RWOnly rwOnly = new RWOnly();
+//        rwOnly.privateVal = 10;
+//        System.out.println(rwOnly.privateVal);
+//        java: privateVal has private access in com.driver.RWOnly
+        rwOnly.setPrivateVal(10);
+        System.out.println(rwOnly.getPrivateVal());
+    }
+
 }
--- src/main/java/com/driver/RWOnly.java
+++ src/main/java/com/driver/RWOnly.java
@@ -1,5 +1,16 @@
 package com.driver;
 
 public class RWOnly {
-    
+    private  int privateVal = 5;
+
+    public int getPrivateVal() {
+        return privateVal;
+    }
+
+    public void setPrivateVal(int privateVal) {
+        this.privateVal = privateVal;
+    }
+
+
+
 }
